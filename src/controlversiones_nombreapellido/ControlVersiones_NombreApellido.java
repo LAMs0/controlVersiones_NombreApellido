940bb362a1b65ff6cf3c6fde8665c92b8bc465da
Luis López <dg.luislo8@gmail.com>
2025-11-04 17:32:52 -0800

    Creacion del proyecto y clase principal

---
+++ src/controlversiones_nombreapellido/ControlVersiones_NombreApellido.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controlversiones_nombreapellido;
+
+/**
+ *
+ * @author venga
+ */
+public class ControlVersiones_NombreApellido {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
